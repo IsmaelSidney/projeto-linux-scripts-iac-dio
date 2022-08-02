e0f7eda7048d69b9b188a25a551bbff59f6abc69
IsmaelSidney <mael-sf@hotmail.com>
2022-08-02 19:29:25 +0000

    first commit

---
+++ iacl.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "Criando diretorios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando grupos de usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando usuários e adicionando aos grupos..."
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -6 senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -6 senha123) -G GRP_ADM
+useradd debora -m -s /bin/bash -p $(openssl passwd -6 senha123) -G GRP_ADM
+
+useradd joao -m -s /bin/bash -p $(openssl passwd -6 senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -6 senha123) -G GRP_VEN
+useradd roberta -m -s /bin/bash -p $(openssl passwd -6 senha123) -G GRP_VEN
+
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -6 senha123) -G GRP_SEC
+useradd amandaa -m -s /bin/bash -p $(openssl passwd -6 senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -6 senha123) -G GRP_SEC
+
+echo "Especificando permissõesx dos diretórios..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec 
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+
+chmod 777 /publico
+
+echo "Finalizado!!"
+ 
